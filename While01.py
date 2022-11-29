1fecd40076f82e25a40f1e958455edae2c1b70af
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-29 13:09:07 +0500

    While01.py

--- While01.py
+++ While01.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    x=0
+    y=0
+    while x<len(s):
+        if s[x].isdigit():
+            y+=1
+        x+=1
+    return y
+
+print(main("python 2022"))
+
+                
