11b91721bdcaa0f579c3e8609432d4a8197e4909
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-29 18:05:42 +0500

    While06.py

--- While06.py
+++ While06.py
@@ -13,6 +13,6 @@ def main(s):
         if s[x].isalpha():
             if s[x]!='a' and s[x]!='e' and s[x]!='i' and s[x]!='o' and s[x]!='u' and s[x]!='A' and s[x]!='E' and s[x]!='I' and s[x]!='U' and s[x]!='U':
                 y+=1
-            x+=1
+        x+=1
     return y
 print(main("CodeschoolUz"))
