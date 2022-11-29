ff43b92a2d1014540a96112a8c087ef6db9d9088
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-29 13:36:59 +0500

    While06.py

--- While06.py
+++ While06.py
@@ -7,3 +7,12 @@ def main(s):
     Returns:
         int: return answer
     """
+    x=0
+    y=0
+    while x<len(s):
+        if s[x].isalpha():
+            if s[x]!='a' and s[x]!='e' and s[x]!='i' and s[x]!='o' and s[x]!='u' and s[x]!='A' and s[x]!='E' and s[x]!='I' and s[x]!='U' and s[x]!='U':
+                y+=1
+            x+=1
+    return y
+print(main("CodeschoolUz"))
